028f8cb6e92956feb096b4cd8a2b1da83cf07d32
V-Yaroslavskyi <vovasy96@gmail.com>
2017-04-22 18:25:58 +0300

    fixed conf

--- restart.sh
+++ restart.sh
@@ -4,7 +4,7 @@ kill -9 $(cat target/universal/sudobot-1.0/RUNNING_PID )
 rm -r target/universal/sudobot-1.0
 sbt dist
 unzip target/universal/sudobot-1.0.zip
-mv tradehubbot-1.0 target/universal/sudobot-1.0
+mv sudobot-1.0 target/universal/sudobot-1.0
 cd target/universal/
 nohup sudobot-1.0/bin/sudobot -Dhttps.port=9002 -Dhttps.port=9003 -J-Xms2240M -J-Xmx16000M -Dconfig.resource=production.conf -J-server&
 tail -f --lines=100 nohup.out
